0e3313399c846199e1dc53ead9e403ecb40b39f2
user <user@mail.com>
2025-10-27 14:24:09 -0400

    feat: Add CraftingUI to EbitenGame for crafting and recipe management

--- pkg/engine/game.go
+++ pkg/engine/game.go
@@ -43,7 +43,8 @@ type EbitenGame struct {
 	CharacterUI *EbitenCharacterUI
 	SkillsUI    *EbitenSkillsUI
 	MapUI       *EbitenMapUI
-	ShopUI      *ShopUI // Commerce and merchant interaction UI
+	ShopUI      *ShopUI      // Commerce and merchant interaction UI
+	CraftingUI  *CraftingUI  // Crafting and recipe UI
 
 	// Player entity reference (for UI systems)
 	PlayerEntity *Entity

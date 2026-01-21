bf7a1955baa67ba31c4573b1b55e3e03a38aba80
user <user@mail.com>
2026-01-21 17:26:52 -0500

    audit

--- loop.sh
+++ loop.sh
@@ -1,20 +1,7 @@
 #! /usr/bin/env sh
-
-# Number of iterations to perform
-# (must be a positive integer)
-ITER=15
-
-alias copilot="yes n | copilot --model claude-opus-4.5"
-
-#./cmd.sh impl
-rm -fv docs/PLAN.md docs/INTEGRATION_AUDIT.md
-#./cmd.sh integrate
-./cmd.sh fix
-#./cmd.sh wasm
-#./cmd.sh mobile
-for i in $(seq 1 $ITER); do
-    ./cmd.sh auditloop
-done
-
-git clean -fdx .
-./cmd.sh fix
+git commit -am "audit"
+git push origin main
+git pull origin main --no-rebase;
+devloop.sh
+sleep 1m
+breakloop.sh
